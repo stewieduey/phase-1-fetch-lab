69c526c7302afd8766a20d9c6b484b2b50ac70b7
DuMark Allman <dumark.allman@yahoo.com>
2023-04-11 22:53:52 -0400

    [StewieDuey]

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+function fetchBooks(){
+return fetch("https://anapioficeandfire.com/api/books").then((response) => {  
+    //console.log('resolved', response);
+    return response.json();
+}).then(data => { renderBooks(data);
+  //console.log(data);
+}).catch((err) => {
+  //console.log('rejected', err);
+}); 
 }
 
-function renderBooks(books) {
+  // To pass the tests, don't forget to return your fetch!
+  // I think I broke the code.
+
+function renderBooks(data) {
   const main = document.querySelector('main');
-  books.forEach(book => {
+  data.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
